aae900ce235e48afaa1db98f6b7e4bd43a7f8020
salvadorduch <salva.sb001@gmail.com>
2025-09-09 20:03:42 -0300

    Create ConfigurationManager.java

---
+++ PatronObserver/ConfigurationManager.java
@@ -0,0 +1,8 @@
+// ConfigurationManager.java
+public class ConfigurationManager {
+    // Valores simulados de configuración
+    public long getMaxFileSize() {
+        // 10 MB por ejemplo
+        return 10_000_000L;
+    }
+}
